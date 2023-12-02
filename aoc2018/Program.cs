44edda3a7a0e2f5426c32451f3717c02bc17bd39
Luctia <timmerman.luc1999@gmail.com>
2023-12-02 17:16:36 +0100

    Day 1

---
+++ aoc2018/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aoc2018.solutions;
+
+internal class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        new Day01().Part1();
+        new Day01().Part2();
+    }
+}
---
+++ aoc2018/solutions/Day01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2018.solutions
+{
+    internal class Day01 : Day
+    {
+        public override void Part1()
+        {
+            string[] input = GetInputLines();
+            int sum = 0;
+            foreach (string line in input)
+            {
+                sum += int.Parse(line);
+            }
+            Answer(sum);
+        }
+
+        public override void Part2()
+        {
+            string[] input = GetInputLines();
+            List<int> frequencies = new List<int>();
+            int sum = 0;
+            while (true)
+            {
+                foreach (string line in input)
+                {
+                    sum += int.Parse(line);
+                    if (frequencies.Contains(sum))
+                    {
+                        Answer(sum);
+                        return;
+                    }
+                    frequencies.Add(sum);
+                }
+            }
+        }
+    }
+}
